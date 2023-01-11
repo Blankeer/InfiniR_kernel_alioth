a6bdde4a4672eb43e8d50998d05c00049f941fa0
Blanke <blankeeee@gmail.com>
2023-01-11 17:54:42 +0800

    Update build_full.sh

--- build_full.sh
+++ build_full.sh
@@ -11,10 +11,21 @@ sudo apt install -y gcc g++ python make texinfo texlive bc bison build-essential
     libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
     unzip language-pack-zh-hans
 # GCC Cross
-wget "https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz"
-xz -d gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
-tar xf gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar
-mv gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu .gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
+# wget "https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz"
+# xz -d gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz
+# tar xf gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar
+# mv gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu .gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
+
+wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r445002.tar.gz -O "clang-r445002.tar.gz"
+tar -xf clang-r445002.tar.gz -C clang-r445002
+
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.0.0_r15.tar.gz -O "gcc64.tar.gz"
+tar -xf gcc64.tar.gz -C aarch64-linux-android-4.9
+
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.0.0_r15.tar.gz -O "gcc32.tar.gz"
+tar -xf gcc32.tar.gz -C arm-linux-androideabi-4.9
+
+
 # Env
 export ARCH=arm64
 export SUBARCH=arm64
@@ -32,6 +43,9 @@ cd InfiniR_kernel_alioth
 make clean 
 make mrproper 
 # args="-j$(nproc --all) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-" 
-args="-j64 O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-" 
-make ${args} O=out vendor/alioth_defconfig 
+# args="-j64 O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-" 
+
+args="ARCH=arm64 SUBARCH=arm64 CC=clang-r445002/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-4.9/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+
+make -j$(nproc --all) ${args} O=out vendor/alioth_defconfig 
 make ${args} CONFIG_DEBUG_SECTION_MISMATCH=y
